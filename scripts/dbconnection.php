74553aa21ea34e69474234c12b1009dce92aa8a0
icisystem <58477039+icisystem@users.noreply.github.com>
2020-03-18 21:41:48 +0100

    db connection file

---
+++ scripts/dbconnection.php
@@ -0,0 +1,18 @@
+<?php
+class DBConnection{
+   // properties
+   private $host = 'localhost';
+   //  private $dbname = 'bulkmailing_db';
+   //  private $dbuser = 'root';
+   //  private $dbpass = '';
+   private $dbname = 'nglohite_bulk';
+	private $dbuser = 'nglohite_bulk';
+   private $dbpass = '$E}G9TDdlMX7';
+    
+   // class constructor
+   public function __construct(){
+      $dbconn = new mysqli($this->host, $this->dbuser, $this->dbpass, $this->dbname);
+      return $dbconn;
+   }
+}
+?>
