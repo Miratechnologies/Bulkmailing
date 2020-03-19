826e004e8ef39ddc8f6c3c665e9e1a942178fbdf
ebuka <ebukaodini@gmail.com>
2020-03-19 08:48:11 +0100

    authorization interface

--- authorize_campaign.php
+++ authorize_campaign.php
@@ -10,7 +10,7 @@ $model = new DBModel();
 $_SESSION['USER.ACTIVITY'][] = "scheduled list - " . date("d/m/Y h:i a");
 include 'scripts/log.php';
 
-$mails = $model->getAllScheduledEmail();
+$mails = $model->getAllPendingEmailCampaign();
 
 if ($mails['flag'] == true) {
 	$empty = false;
@@ -54,7 +54,7 @@ if ($mails['flag'] == true) {
 
 			<div class="bg-white p-2 mx-0 my-0">
 				<div class="mx-lg-5 mx-sm-auto text-dark display-4">
-					Scheduled Mails
+					Pending Mail Campaigns
 					<div class="float-lg-right float-sm-none btn obejor-bg-dark text-white my-lg-3 cursor-pointer" onclick="location.href='campaign.email.schedule.php';">Schedule another Mail</div>
 				</div>
 				<div class="clearfix"></div>
@@ -67,14 +67,14 @@ if ($mails['flag'] == true) {
 						echo "
 						<div class='alert alert-success alert-dismissible fade show'>
 							<button type='button' class='close alert-dismissible' data-dismiss='alert'>&times;</button>
-							<strong>Success!</strong> The scheduled mail was {$_GET['action']}.
+							<strong>Success!</strong> The mail campaign was {$_GET['action']}.
 						</div>
 						";
 					} elseif (isset($_GET['update']) && $_GET['update'] == 'false') {
 						echo "
 						<div class='alert alert-danger alert-dismissible fade show'>
 							<button type='button' class='close alert-dismissible' data-dismiss='alert'>&times;</button>
-							<strong>Sorry!</strong> The scheduled mail was not {$_GET['action']}.
+							<strong>Sorry!</strong> The mail campaign was not {$_GET['action']}.
 						</div>
 						";
 					}
@@ -87,11 +87,7 @@ if ($mails['flag'] == true) {
 								<th>#</th>
 								<th>Subject</th>
 								<th>Sender</th>
-								<th>Date&nbsp;Scheduled</th>
-								<th>Scheduled&nbsp;Date</th>
-								<th>Scheduled&nbsp;Time</th>
-								<!-- <th>Intervals</th> -->
-								<!-- <th>Last&nbsp;Sent</th> -->
+								<th>Recipients</th>
 								<th>Status</th>
 								<th>Action</th>
 							</tr>
@@ -104,39 +100,20 @@ if ($mails['flag'] == true) {
 									foreach ($mails as $mail) {
 
 										// if status is active
-										if ($mail['status'] == "ACTIVE") {
+										if ($mail['status'] == "Pending") {
 											// it can be set to cancel the schedule
-											$action = "<button class='btn btn-sm obejor-bg-dark text-light' onclick=\"location.href='scripts/update_schedule_status.php?id={$mail['scheduler_id']}&status=CANCEL'\">Cancel</button>";
-										}
-										// else if the status is cancelled
-										elseif ($mail['status'] == "CANCEL") {
-											// it can be set back to activate
-											$action = "<button class='btn btn-sm obejor-bg-dark text-light' onclick=\"location.href='scripts/update_schedule_status.php?id={$mail['scheduler_id']}&status=ACTIVE'\">Activate</button>";
+											$action = "<button class='btn btn-sm obejor-bg-dark text-light' onclick=\"location.href='scripts/send_mail.php?id={$mail['mail_id']}&status=Authorize'\">Authorize</button>";
 										}
 										// else the status is now either SENT or EXPIRED, hence no action
 										else {
-											$action = "";
+											$action = "<button class='btn btn-sm obejor-bg-dark text-light' onclick=\"location.href='scripts/send_mail.php?id={$mail['mail_id']}&status=Reject'\">Reject</button>";
 										}
-
-										$date1 = date_create($mail['date_scheduled']);
-										$date1 = date_format($date1, "d/m/Y");
-
-										$date2 = date_create($mail['schedule_date']);
-										$date2 = date_format($date2, "d/m/Y");
-
-										$date3 = date_create($mail['schedule_time']);
-										$date3 = date_format($date3, "h:ia");
-
-										// $date4 = date_create($mail['last_sent']);
-										// $date4 = date_format($date4, "d/m/Y h:ia");
 
 										echo "<tr class='text-dark'>
 											<td>{$count}</td>
 											<td>{$mail['subject']}</td>
 											<td>{$mail['sender']}</td>
-											<td>{$date1}</td>
-											<td>{$date2}</td>
-											<td>{$date3}</td>
+											<td>{$mail['recipients']}</td>
 											<td>{$mail['status']}</td>
 											<td>
 												{$action}
@@ -147,7 +124,7 @@ if ($mails['flag'] == true) {
 									}
 								} else {
 									echo '<tr>
-										<td colspan="7">No User Exists</td>
+										<td colspan="6">No Campaign Pending Authorization</td>
 									</tr>';
 								} 
 							?>
