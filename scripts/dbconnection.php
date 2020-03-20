be6a1233d4518b77b5f551dcfd0f86e5a243f74d
icisystem <58477039+icisystem@users.noreply.github.com>
2020-03-20 13:48:18 +0100

    Update dbconnection.php

--- scripts/dbconnection.php
+++ scripts/dbconnection.php
@@ -2,12 +2,12 @@
 class DBConnection{
    // properties
    private $host = 'localhost';
-    private $dbname = 'bulkmailing_db';
-    private $dbuser = 'root';
-    private $dbpass = '';
-   // private $dbname = 'nglohite_bulk';
-	// private $dbuser = 'nglohite_bulk';
-   // private $dbpass = '$E}G9TDdlMX7';
+//     private $dbname = 'bulkmailing_db';
+//     private $dbuser = 'root';
+//     private $dbpass = '';
+   private $dbname = 'nglohite_bulk';
+	private $dbuser = 'nglohite_bulk';
+   private $dbpass = '$E}G9TDdlMX7';
     
    // class constructor
    public function __construct(){
