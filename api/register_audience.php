a9b91d5bc385a97f40fb898e43b1da05acbd278c
ebuka <ebukaodini@gmail.com>
2020-03-21 19:34:35 +0100

    bugfix

--- api/register_audience.php
+++ api/register_audience.php
@@ -61,7 +61,7 @@ $SMTP_Validator->debug = false;
 $results = $SMTP_Validator->validate(array($email), $sender);
 
 // check results
-// if ($results[$email] === true) {
+if ($results[$email] === true) {
    include '../scripts/dbmodel.php';
    
    $model = new DBModel();
@@ -79,7 +79,7 @@ $results = $SMTP_Validator->validate(array($email), $sender);
       $request == "Application" ? exit(header("location: ../audience.php?errmsg=Customer already exists on our newsletter.")) : die(json_encode(["flag"=>false,"data"=>"Customer could not subscribe to newsletter."]));
    }
 
-// } else {
-//    // ignore the email and continue unto the next record
-//    $request == "Application" ? exit(header("location: ../audience.php?errmsg=Email does not exist.")) : die(json_encode(["flag"=>false,"msg"=>"Email does not exist."]));
-// }
+} else {
+   // ignore the email and continue unto the next record
+   $request == "Application" ? exit(header("location: ../audience.php?errmsg=Email does not exist.")) : die(json_encode(["flag"=>false,"msg"=>"Email does not exist."]));
+}
